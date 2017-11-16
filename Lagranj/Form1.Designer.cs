f711f810a14123c57cc710db25622ff7005b800f
Arstanaly Rysbekov <rysbekov_a@au.kg>
2017-11-16 11:49:08 +0600

    addd everything

---
+++ Lagranj/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace Integration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxE = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelError = new System.Windows.Forms.Label();
+            this.comboBoxMethod = new System.Windows.Forms.ComboBox();
+            this.comboBoxTask = new System.Windows.Forms.ComboBox();
+            this.comboBoxNodes = new System.Windows.Forms.ComboBox();
+            this.ctrl = new ZedGraph.ZedGraphControl();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(709, 707);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(326, 62);
+            this.button1.TabIndex = 0;
+            this.button1.Text = ">>";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(775, 665);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(201, 26);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Чтобы ознакомиться с данной темой,\r\n           нажмите на кнопку";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBoxE);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.labelError);
+            this.groupBox1.Controls.Add(this.comboBoxMethod);
+            this.groupBox1.Controls.Add(this.comboBoxTask);
+            this.groupBox1.Controls.Add(this.comboBoxNodes);
+            this.groupBox1.Controls.Add(this.ctrl);
+            this.groupBox1.Location = new System.Drawing.Point(12, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(866, 451);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(6, 165);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Параметр E";
+            // 
+            // comboBoxE
+            // 
+            this.comboBoxE.FormattingEnabled = true;
+            this.comboBoxE.Items.AddRange(new object[] {
+            "0,5",
+            "0,25",
+            "0,125",
+            "0,0625",
+            "0,03125",
+            "0,015625",
+            "0,0078125"});
+            this.comboBoxE.Location = new System.Drawing.Point(6, 181);
+            this.comboBoxE.Name = "comboBoxE";
+            this.comboBoxE.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxE.TabIndex = 13;
+            this.comboBoxE.SelectedIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 367);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 12;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button3.Location = new System.Drawing.Point(6, 413);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 32);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Draw";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label5.Location = new System.Drawing.Point(6, 16);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Задача";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label6.Location = new System.Drawing.Point(6, 67);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Метод";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(6, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Число узлов";
+            // 
+            // labelError
+            // 
+            this.labelError.AutoSize = true;
+            this.labelError.ForeColor = System.Drawing.Color.Tomato;
+            this.labelError.Location = new System.Drawing.Point(6, 351);
+            this.labelError.Name = "labelError";
+            this.labelError.Size = new System.Drawing.Size(124, 13);
+            this.labelError.TabIndex = 4;
+            this.labelError.Text = "Ошибка интерполяции:";
+            // 
+            // comboBoxMethod
+            // 
+            this.comboBoxMethod.FormattingEnabled = true;
+            this.comboBoxMethod.Items.AddRange(new object[] {
+            "Одностадийный метод",
+            "Двухстадийный метод",
+            "Трехстадийный метод"});
+            this.comboBoxMethod.Location = new System.Drawing.Point(6, 83);
+            this.comboBoxMethod.Name = "comboBoxMethod";
+            this.comboBoxMethod.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxMethod.TabIndex = 3;
+            this.comboBoxMethod.SelectedIndex = 0;
+            // 
+            // comboBoxTask
+            // 
+            this.comboBoxTask.FormattingEnabled = true;
+            this.comboBoxTask.Items.AddRange(new object[] {
+            "Задача №2",
+            "Задача №8",
+            "Задача №12.1",
+            "Задача №13.3"});
+            this.comboBoxTask.Location = new System.Drawing.Point(9, 32);
+            this.comboBoxTask.Name = "comboBoxTask";
+            this.comboBoxTask.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxTask.TabIndex = 2;
+            this.comboBoxTask.SelectedIndex = 0;
+            // 
+            // comboBoxNodes
+            // 
+            this.comboBoxNodes.FormattingEnabled = true;
+            this.comboBoxNodes.Items.AddRange(new object[] {
+            "3",
+            "7",
+            "9",
+            "17",
+            "33",
+            "129"});
+            this.comboBoxNodes.Location = new System.Drawing.Point(6, 132);
+            this.comboBoxNodes.Name = "comboBoxNodes";
+            this.comboBoxNodes.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxNodes.TabIndex = 0;
+            this.comboBoxNodes.SelectedIndex = 0;
+            // 
+            // ctrl
+            // 
+            this.ctrl.BackColor = System.Drawing.Color.Cyan;
+            this.ctrl.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ctrl.Location = new System.Drawing.Point(159, 13);
+            this.ctrl.Margin = new System.Windows.Forms.Padding(4);
+            this.ctrl.Name = "ctrl";
+            this.ctrl.ScrollGrace = 0D;
+            this.ctrl.ScrollMaxX = 0D;
+            this.ctrl.ScrollMaxY = 0D;
+            this.ctrl.ScrollMaxY2 = 0D;
+            this.ctrl.ScrollMinX = 0D;
+            this.ctrl.ScrollMinY = 0D;
+            this.ctrl.ScrollMinY2 = 0D;
+            this.ctrl.Size = new System.Drawing.Size(700, 431);
+            this.ctrl.TabIndex = 11;
+            this.ctrl.Load += new System.EventHandler(this.ctrl_Load);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkRed;
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(772, 794);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(204, 25);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Выход";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(890, 504);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Решение обыкновенных дифференциальных уравнений";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBoxMethod;
+        private System.Windows.Forms.ComboBox comboBoxTask;
+        private System.Windows.Forms.ComboBox comboBoxNodes;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelError;
+        private System.Windows.Forms.Button button3;
+        private ZedGraph.ZedGraphControl ctrl;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxE;
+    }
+}
+
---
+++ Lagranj/Form1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+using ZedGraph;
+namespace Integration
+{
+    public partial class Form1 : Form
+    {
+        private double e;
+        private double n;
+        private double tao;
+        private double uh0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            groupBox1.Visible = true;
+        }
+
+        public double function(double x)
+        {
+            switch (comboBoxTask.SelectedIndex)
+            {
+                case 0: //Задача #2
+                    return (2.0 + (x * x) / (2.0 * e)) * Math.Exp(-x * x / e);
+                case 1: //Задача #8
+                    return 1.0 / (Math.Sqrt(1.0 + 3.0 * Math.Exp(x * x / e)));
+                case 2: //Task #12.1
+                    return 1.0/ (x - e + (1.0 + e) * Math.Exp(-x /e));
+                case 3: //Задача 13.3
+                    return (-3.0 + Math.Exp(x / e)) / (3.0 + Math.Exp(x / e));
+                default:
+                    return 0;
+            }
+        }
+
+        private double f(double u, double t)
+        {
+            switch (comboBoxTask.SelectedIndex)
+            {
+                case 0: //Task #2
+                    return (t * Math.Exp(-t * t / e) - 2.0 * t * u) / e;
+                case 1: //Task #8
+                    return (t * Math.Pow(u,3) - t * u) / e;
+                case 2: //Task #12.1
+                    return (u - t * u * u) / e;
+                case 3: //Task #13.3
+                    return ((u + 3.0) * (1.0 - u)) / (4.0 * e);
+                default:
+                    return 0;
+            }
+        }
+
+      
+       private double firststage(double x) //Одностадийный метод
+        {
+            if (x == 0)
+            {
+                return uh0;
+            }
+            else
+            {
+                uh0 = uh0 + tao * f(uh0, x);
+                return uh0;
+            }
+        }
+
+        private double secondstage(double x) //Двухстадийный метод
+        {
+
+            if (x == 0)
+            {
+                return uh0;
+            }
+            else
+            {
+                //лямбда равна одному
+                double temp = f(uh0, x);
+                double yh = uh0 + tao * temp;
+                double uhnext = uh0 + tao * (0.5 * temp + 0.5 * f(yh, (x + tao)));
+                uh0 = uhnext;
+                return uh0;
+            }
+        }
+
+        private double thirdstage(double x) //Трехстадийный метод
+        {
+            if (x == 0)
+            {
+                return uh0;
+            }
+            else
+            {
+                //тета равна одному
+                double temp = f(uh0, x);
+                double yh1 = uh0 + ((2.0 * tao) / 3.0) * temp;
+                double temp2 = f(yh1, x + (2.0 * tao) / 3.0);
+                double yh2 = uh0 + ((2.0 * tao) / 3.0) * ((1 - (3.0 / 8.0)) * temp + (3.0 / 8.0) * temp2);
+                double uhnext = uh0 + tao * ((0.25 * temp + (3.0 / 4.0 - 1) * temp2 + f(yh2, x + (2.0 * tao / 3.0))));
+                uh0 = uhnext;
+                return uh0;
+            }
+        }
+
+
+        private void maxError()
+        {
+            double num = 0;
+            double denum = 0;
+            double temp;
+            for(double x = 0; x < 1; x+=tao)
+            {
+                temp = Math.Abs(function(x));
+
+                if (temp > denum) denum = temp;
+                if(comboBoxMethod.SelectedIndex == 0)
+                    temp = Math.Abs(temp - firststage(x));
+                else if(comboBoxMethod.SelectedIndex == 1)
+                    temp = Math.Abs(temp - secondstage(x));
+                else
+                    temp = Math.Abs(temp - thirdstage(x));
+
+                if (temp > num) num = temp;
+                
+            }
+           
+            textBox1.Text = Convert.ToString(Math.Abs(num / denum * 100.0));
+
+        }
+
+
+        public void Draw()
+        {
+            ctrl.GraphPane.YAxis.IsVisible = true;
+            PointPairList list1 = new PointPairList();
+            GraphPane myPane = ctrl.GraphPane;
+            myPane.CurveList.Clear();
+
+            e = Convert.ToDouble(comboBoxE.Text); //параметр E
+            n = Convert.ToDouble(comboBoxNodes.Text); //число узлов
+            tao = 1.0 / (n - 1.0); // шаг сетки
+           
+
+            for (double x = 0.0; x <= 1; x += 0.0001)
+            {
+                list1.Add(x, function(x));
+            }
+
+            LineItem line1 = myPane.AddCurve("Тестовая функция", list1, Color.Black, SymbolType.None);
+            line1.Line.IsVisible = true;
+            line1.Symbol.Fill.Color = Color.Black;
+            line1.Symbol.Fill.Type = FillType.Solid;
+            line1.Symbol.Size = 2.0f;
+            ctrl.AxisChange();
+            ctrl.Invalidate();
+            myPane.Title.Text = " ";
+
+
+            
+            PointPairList list2 = new PointPairList();
+
+
+            if (comboBoxTask.SelectedIndex == 0) uh0 = 2; //Задача #2
+            else if (comboBoxTask.SelectedIndex == 1) uh0 = 0.5; //Задача #8
+            else if (comboBoxTask.SelectedIndex == 2) uh0 = 1; //Task #12.1
+            else if (comboBoxTask.SelectedIndex == 3) uh0 = -0.5; //Задача 13.3
+
+
+            if (comboBoxMethod.SelectedIndex == 0) {
+                for (double x = 0; x <= 1; x += tao)
+                {
+                    list2.Add(x, firststage(x));
+                }
+            }
+            else if(comboBoxMethod.SelectedIndex == 1)
+            {
+                for (double x = 0; x <= 1; x += tao)
+                {
+                    list2.Add(x, secondstage(x));
+                }
+            }
+            else
+            {
+                for (double x = 0; x <= 1; x += tao)
+                {
+                    list2.Add(x, thirdstage(x));
+                }
+            }
+
+            LineItem line2 = myPane.AddCurve("Метод Рунге-Кутта", list2, Color.Red, SymbolType.None);
+            line2.Line.IsVisible = true;
+            line2.Symbol.Fill.Color = Color.Red;
+            line2.Symbol.Fill.Type = FillType.Solid;
+            line2.Symbol.Size = 6.0f;
+            line2.IsSelectable = true;
+            line2.IsOverrideOrdinal = true;
+            ctrl.AxisChange();
+            ctrl.Invalidate();
+
+
+            maxError();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            groupBox1.Visible = true;
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(60);
+                this.Opacity = this.Opacity - 0.1;
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(60);
+                this.Opacity = this.Opacity + 0.1;
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ctrl_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
